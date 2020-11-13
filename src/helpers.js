c90099fca7b6e5990cbed03b79e84ba07ebe7f79
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-11-13 18:57:36 +0800

    ADD functionality to add project

---
+++ src/helpers.js
@@ -0,0 +1,25 @@
+const helpr = (() => {
+  const addChildren = (parent, items) => {
+    items.forEach(el => {
+      parent.appendChild(el);
+    });
+  };
+
+  const textEl = (elType, elText) => {
+    const res = document.createElement(elType);
+    res.innerHTML = elText;
+
+    return res;
+  };
+
+  const classyDiv = (className) => {
+    const res = document.createElement('div');
+    res.setAttribute('class', className);
+
+    return res;
+  };
+
+  return { addChildren, textEl, classyDiv };
+})();
+
+export default helpr;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+//import helpr from './helpers'
+
+const helpr = (() => {
+  const addChildren = (parent, items) => {
+    items.forEach(el => {
+      parent.appendChild(el);
+    });
+  };
+
+  const textEl = (elType, elText) => {
+    const res = document.createElement(elType);
+    res.innerHTML = elText;
+
+    return res;
+  };
+
+  const classyDiv = (className) => {
+    const res = document.createElement('div');
+    res.setAttribute('class', className);
+
+    return res;
+  };
+
+  return { addChildren, textEl, classyDiv };
+})();
+
+
 const newTask = (title, description = '', dueDate = '', priority = '', completed = false, project = 'default') => ({
   title: title,
   description: description,
@@ -7,14 +34,35 @@ const newTask = (title, description = '', dueDate = '', priority = '', completed
   project: 'default'
 })
 
-// list containing projects
-const projects = []
-
 // title (str), tasks (array)
 const newProject = (title, tasks) => ({
   title: title,
   tasks: tasks
 })
+
+// list containing projects
+const projects = []
+const projectList = document.querySelector('.project-list')
+
+// DOM manipulation
+
+const displayProject = (project) => {
+  const p = helpr.classyDiv('project')
+  const projectTitle = helpr.textEl('h3', project.title)
+  const taskList = document.createElement('ul')
+  const taskForm = document.createElement('form')
+  let textIn = document.createElement('input')
+  textIn.setAttribute('type', 'text')
+  let formSub = document.createElement('input')
+  formSub.setAttribute('type', 'submit')
+  formSub.setAttribute('value', 'Add Task')
+  helpr.addChildren(taskForm, [textIn, formSub])
+  helpr.addChildren(p, [projectTitle, taskList, taskForm])
+  projectList.appendChild(p)
+}
+
+
+
 
 // module structure: tasks, projects, UI
 
@@ -38,10 +86,16 @@ const createProject = document.querySelector('.create-project-btn')
 createProject.addEventListener('click', (e) => {
   const projectName = document.querySelector('.new-project-name').value
   if (projectName.length > 0) {
-    projects.push(projectName)
-    console.log(projects)
+    let p = newProject(projectName)
+    projects.push(p)
+
+    projects.forEach(displayProject)
+    // for (let i = 0; i < projects.length; i++) {
+    //   displayProject(projects[i])
+    // }
   }
 })
+
 
 
 /* task CRUD
