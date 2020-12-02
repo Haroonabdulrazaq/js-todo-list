14d5512f6aa9a7aa828b7a3054b0ff2744797f45
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-02 10:52:47 +0100

    Hide project form when addProject is clicked

--- src/ui.test.js
+++ src/ui.test.js
@@ -5,3 +5,14 @@ test('map calls its argument with a non-null argument', () => {
   [1].map(x => initUI(x));
   expect(initUI).toBeCalledWith(expect.anything());
 });
+
+test('It shoukd hide the project form on click',()=>{
+
+  document.body.innerHTML = `<button class ='add-project'>Add Project</button>
+                              <form class='new-project-form hide'></form>`
+
+  const addProject = document.querySelector('.add-project');
+  const projectForm = document.querySelector('.new-project-form');
+  addProject.click();
+  expect(projectForm.innerHTML).toBe(``)
+})
